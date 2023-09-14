59240f0ff4e49b3e7b750c879ff6b79cdbd0a704
Ana Ramirez <aramirez.cac@gmail.com>
2023-09-13 20:41:45 -0400

    usando estados para buscador y el contador

--- src/App.js
+++ src/App.js
@@ -10,14 +10,34 @@ import React from 'react';
 const   defaultTodos = [
   {text:"Limpiar el patio", completed:true},
   {text:"Lavar los cubiertos", completed:false} ,
-  {text:"Cocinar bien rico", completed:true}
+  {text:"Cocinar bien rico", completed:true},
 ]
 
 function App() {
+
+  //aca declaramos para el conteo de TODO
+  //le pasamos por defecto los defaultTodos declarads mas arriba
+  const [todos, setTodos] = React.useState(defaultTodos);
+
+    const completedTodos = todos.filter(todo => todo.completed).length; //que solamente me traiga los todo con completed true
+    const totalTodos=todos.length;//obtener el total
+
+   //se le pasa como arrays porque tiene en cuenta 2 parametros
+   //porque el estado no solo de consume si no tambien se actualiza
+  //ej.(la variable no es fijo)ejemplo:  estado => el que registra el usuario, setEstado => en donde se actualiza lo que va registrando el usuario
+  const [search, setSearch] = React.useState('');
+  console.log('lo que busca el usuario es: '+search);
+
+
   return (  
     <>
-      <TodoCounter total={15} completed={10}/>
-      <TodoSearch/>
+      <TodoCounter total={totalTodos} completed={completedTodos}/>
+      <TodoSearch
+      //aca le pasamos las variables a TodoSearch
+      search={search}
+      setSearch={setSearch}
+
+      />
 
       <TodoList>
         {defaultTodos.map(todo =>(
--- src/componentes/TodoSearch/TodoSearch.js
+++ src/componentes/TodoSearch/TodoSearch.js
@@ -1,10 +1,24 @@
+import React from "react";
 import "./TodoSearch.css";
 
 
-function TodoSearch(){
+function TodoSearch({
+  //aca es en donde recibimos las variables de la clase App.js
+  search, 
+  setSearch
+}){
+ 
     return (
       <div className="search-container">
-        <input placeholder="Ingresar aqui la tarea"/>
+        <input 
+        placeholder="Ingresar aqui la tarea"
+        className="TodoSearch"
+        value={search}//el valor de la busqueda
+        onChange={(event) => {
+          console.log('escribiste en el search')
+          setSearch(event.target.value);
+        }}
+        />
      </div>
     );
   }
