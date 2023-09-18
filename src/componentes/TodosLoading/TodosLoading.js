44b207a032faf996b3a332ec2f645f4ac8ff69bb
Ana Ramirez <aramirez.cac@gmail.com>
2023-09-17 20:10:26 -0400

    version con un estado de tres barras de carga

--- src/componentes/TodosLoading/TodosLoading.js
+++ src/componentes/TodosLoading/TodosLoading.js
@@ -5,9 +5,23 @@ import "./TodosLoading.css"
 function TodosLoading(){
  
     return (
-        <div className="loading-box">
-        <div className="loading-animation"></div>
-    </div>
+        <>
+        <div className="item-container-skeleton">
+            <div className="img-round skeleton"></div>
+            <p className="skeleton skeleton-text__body skeleton-text"></p>
+            <div className="img-round skeleton"></div>
+        </div>
+         <div className="item-container-skeleton">
+            <div className="img-round skeleton"></div>
+            <p className="skeleton skeleton-text__body skeleton-text"></p>
+            <div className="img-round skeleton"></div>
+         </div>
+         <div className="item-container-skeleton">
+            <div className="img-round skeleton"></div>
+            <p className="skeleton skeleton-text__body skeleton-text"></p>
+            <div className="img-round skeleton"></div>
+         </div>
+         </>
 );
   }
 
