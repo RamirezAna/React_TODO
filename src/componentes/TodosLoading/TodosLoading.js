8e31aa05e3a299de4ef99b03b706b7c3568c7726
Ana Ramirez <aramirez.cac@gmail.com>
2023-09-17 20:14:16 -0400

    version con un estado de carga animado de barra

--- src/componentes/TodosLoading/TodosLoading.js
+++ src/componentes/TodosLoading/TodosLoading.js
@@ -6,23 +6,14 @@ function TodosLoading(){
  
     return (
         <>
-        <div className="item-container-skeleton">
-            <div className="img-round skeleton"></div>
-            <p className="skeleton skeleton-text__body skeleton-text"></p>
-            <div className="img-round skeleton"></div>
-        </div>
-         <div className="item-container-skeleton">
-            <div className="img-round skeleton"></div>
-            <p className="skeleton skeleton-text__body skeleton-text"></p>
-            <div className="img-round skeleton"></div>
-         </div>
-         <div className="item-container-skeleton">
-            <div className="img-round skeleton"></div>
-            <p className="skeleton skeleton-text__body skeleton-text"></p>
-            <div className="img-round skeleton"></div>
-         </div>
-         </>
-);
+          <li className={`TodoItem-loading cat-0`}>
+            
+            <p className={`TodoItem-p-loading`}>
+            <span className="loader-1"></span>
+            </p>
+          </li>
+        </>
+      );
   }
 
   export {TodosLoading};
